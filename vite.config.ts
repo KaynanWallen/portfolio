46a213b82f0a15c8e518aefb13e109277c4695c9
kaynanwallen <wallenkaynan@gmail.com>
2025-03-23 16:46:54 -0300

    fix: remove vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,35 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { flatRoutes } from "remix-flat-routes";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+installGlobals({ nativeFetch: true });
+
+var plugins = [
+  remix({
+    ignoredRouteFiles: ["**/.*", "**/*.test.{ts,tsx}"],
+    routes: async (defineRoutes) => {
+      return flatRoutes("routes", defineRoutes);
+    },
+    future: {
+      v3_fetcherPersist: true,
+      v3_relativeSplatPath: true,
+      v3_throwAbortReason: true,
+      v3_singleFetch: true,
+      v3_lazyRouteDiscovery: true,
+    },
+  }),
+  tsconfigPaths(),
+];
 
 export default defineConfig({
   server: {
     host: "127.0.0.1",
     cors: false,
-    port: 5555,
     hmr: {
       host: "127.0.0.1",
     },
   },
-  plugins: [
-    remix({
-      routes: async (defineRoutes) => {
-        return flatRoutes("routes", defineRoutes);
-      },
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: plugins,
 });
